8558bfef781ac4472260ed9085023cd6cd955ee2
John Sanders <jws121295@gmail.com>
2017-02-20 00:20:04 +0000

    vanilla http server

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
-console.log("hi there!");
+var http = require("http");
 
-var x = 25;
-var y = x+5;
-console.log(y);
-
-function procrastinate(){
-    console.log("I'll do this tomorrow");
-}
-procrastinate();
-procrastinate();
-procrastinate();
-
-function procrastinate2(future){
-    console.log("I'll do this in "+future+" minutes");
-}
-procrastinate2(5);
-
-console.log(procrastinate2);
-
-if(5 < 0){
-    console.log("bro do you even math");
-} else {
-    console.log("I'm probably a sane person.");
+function requestHandler(requestDetails, response){
+    response.write("Hello");
+    response.end();
 }
 
-var sanity = false;
-if(sanity == true){
-    console.log("HAHA jk");
-    sanity = false;
-}
+var server = http.createServer(requestHandler);
+
+var port = process.env.PORT;
+server.listen(port);
