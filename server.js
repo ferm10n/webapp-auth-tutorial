5821a5d9f21cc08c2a5f7d392910d54f4eff9ba9
John Sanders <jws121295@gmail.com>
2017-02-20 00:29:42 +0000

    Router

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require("express");
 var app = express();
 
 var staticHandler = express.static("public");
+var publicReqHandler = express.Router();
+publicReqHandler.use(staticHandler);
 
-app.use(staticHandler);
+app.use(publicReqHandler);
 
 var port = process.env.PORT;
 app.listen(port);
